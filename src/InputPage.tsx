550c5d8372232bd061f5bbf5320d477f81de02d9
Netsbump <levasseur.sten@gmail.com>
2023-04-25 23:10:13 +0200

    update css and structure html again

--- src/InputPage.tsx
+++ src/InputPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import FullScreenButton from './FullScreenButton';
 import styles from './InputPage.module.css';
 
 interface InputPageProps {
@@ -15,23 +16,51 @@ export const InputPage: React.FC<InputPageProps> = ({ onSubmit }) => {
 
   return (
     <div className={styles.container}>
-      <label>TEMPO</label>
-      <input
-        className={styles.input}
-        type="text"
-        value={tempo.join('/')}
-        onChange={(e) => setTempo(e.target.value.split('/').map(Number) as [number, number, number, number])}
-      />
-      <label>REPETITIONS</label>
-      <input
-        className={styles.input}
-        type="number"
-        value={repetitions}
-        onChange={(e) => setRepetitions(parseInt(e.target.value))}
-      />
-      <button className={styles.submitButton} onClick={handleSubmit}>
-        START
-      </button>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <h1 className={styles.title}>TEMPO</h1>
+        </div>
+        <FullScreenButton />
+      </header>
+      <main>
+        <div className={styles.tempoContainer}>
+          {tempo.map((value, index) => (
+            <div key={index} className={styles.inputWrapper}>
+              <span className={styles.info}>{index % 2 === 0 ? "WORK" : "PAUSE"}</span>
+              <input
+                key={index}
+                className={styles.input}
+                type="number"
+                value={value}
+                onChange={(e) => {
+                  const newTempo = [...tempo];
+                  newTempo[index] = parseInt(e.target.value);
+                  setTempo(newTempo as [number, number, number, number]);
+                }}
+              />
+              <span className={styles.duration}>SECONDS</span>
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <span className={styles.info}>FOR</span>
+          <input
+            className={styles.input}
+            type="number"
+            value={repetitions}
+            onChange={(e) => setRepetitions(parseInt(e.target.value))}
+          />
+          <span className={styles.duration}>ROUND</span>
+        </div>
+        
+        <div className={styles.inputWrapper}>
+        <button className={styles.submitButton} onClick={handleSubmit}>
+          START
+        </button>
+        </div>
+
+      </main>
     </div>
   );
 };
