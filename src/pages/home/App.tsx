f39a3b19951b915307d70e54fc04ba0ee9056de8
Netsbump <levasseur.sten@gmail.com>
2023-05-15 22:11:42 +0200

    add fullscreen button on homepage + input Rest

--- src/pages/home/App.tsx
+++ src/pages/home/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {
   createHashRouter,
-  createBrowserRouter,
-  createRoutesFromElements,
   RouterProvider,
   Route,
   Link,
 } from 'react-router-dom';
+import { FullScreenButton } from '../../components/fullScreenButton/FullScreenButton';
 import { InputPage } from '../tempoApp/setTimer/InputPage';
 import { TempoPage } from '../tempoApp/displayTimer/TempoPage';
 import styles from './App.module.css';
 
 const Menu : React.FC = () => {
   return (
-    <div className="App"> 
+    <div className="App">
+      <header>
+        <FullScreenButton />
+      </header>
       <main className={styles.main}>
         <h1>Choose your timer</h1>
         <nav>
@@ -33,22 +35,15 @@ const Menu : React.FC = () => {
             <li>
               <Link to="emom-input">EMOM</Link>
             </li>
+            <li>
+              <Link to="rest-input">REST</Link>
+            </li>
           </ul>
         </nav>
       </main>
     </div>
   )
 } 
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <>
-//     <Route path="/" element={<Menu />}/>
-//     <Route path="tempo-input" element={<InputPage />} />
-//     <Route path="tempo" element={<TempoPage />} />
-//     </>
-//   )
-// );
 
 const router = createHashRouter([
   {
