1ab38ea454c06d3775534c2d7cae4b12c21fd8a4
Netsbump <levasseur.sten@gmail.com>
2023-07-01 16:34:32 +0200

    add inputEmom

--- src/components/inputTypes/inputEmom/InputEmom.tsx
+++ src/components/inputTypes/inputEmom/InputEmom.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { CustomInput } from '../../customInput/CustomInput';
+import styles from './InputEmom.module.css';
 
 type EmomState = {
-    time: number;
+    minutes: number,
+    numberExercises: number,
 };
 
 interface InputEmomProps {
@@ -11,11 +13,43 @@ interface InputEmomProps {
 
 export const InputEmom : React.FC<InputEmomProps> = ({ onStateChange }) => {
 
-    return (
+    const [minutes, setMinutes] = useState<number>(12)
+    const [numberExercises, setNumberExercises] = useState<number>(3)
 
-        <div>
-            Custom Imput Emom
+    const handleMinutesChange = (newValue : number) => {
+        setMinutes(newValue);
+        onStateChange({ minutes: newValue, numberExercises });
+    }
+
+    const handleNumberExercicesChange = (newValue : number) => {
+        setNumberExercises(newValue);
+        onStateChange({ minutes, numberExercises : newValue});
+    }
+
+    useEffect(() => {
+        onStateChange({ minutes, numberExercises });
+    }, []);
+
+    return (
+    <div className={styles.container}>
+        <div className={styles.inputsContainer}>
+            <div>
+                <CustomInput
+                    label="FOR"
+                    defaultValue={minutes}
+                    onChange={handleMinutesChange}
+                    unit="MINUTES"
+                />
+                <CustomInput
+                    label="DO"
+                    defaultValue={numberExercises}
+                    onChange={handleNumberExercicesChange}
+                    unit="EXERCISES"
+                />
+            </div>
         </div>
+    </div>
+
     )
 
 }
